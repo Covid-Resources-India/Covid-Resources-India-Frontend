b0427dbab9a26c34d86d3ff258192a3eba120b6d
Subhajit Nandi <nandisubhajit666@gmail.com>
2021-04-24 16:57:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<h1 className="text-4xl flex justify-center h-screen items-center">
+			Tailwind configured react app
+		</h1>
+	);
+}
+
+export default App;
